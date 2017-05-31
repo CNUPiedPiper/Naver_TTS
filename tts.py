8a0b6a2e247d6e18a841610aeb457dd7a920947c
Yoon-jae <yoonjae.cho92@gmail.com>
2017-05-31 17:34:52 +0900

    Setting play sound directly

--- tts.py
+++ tts.py
@@ -2,39 +2,48 @@
 import os
 import sys
 import urllib2
-
+from pygame import mixer, time
 import datetime
 
 from urllib2 import urlopen, Request
 from urlparse import urlparse
 
 class tts:
-	def __init__(self):
-		self.client_id = "YOUR_CLIENT_ID"
-		self.client_secret = "YOUR_CLIENT_SECRET"
+    def __init__(self):
+        self.client_id = "YOUR_CLIENT_ID"
+	self.client_secret = "YOUR_CLIENT_SECRET"
+        self.client_id = "_slnCVOnBr5DTve8oVP_"
+        self.client_secret ="Kw3rkrP5EO"
+
+    def play_sound(self, file_path):
+        mixer.init(frequency=16000, buffer=24000)
+        mixer.music.load(file_path)
+        mixer.music.play(loops=2)
+        while mixer.music.get_busy():
+            time.Clock().tick(100)
+
 		
-	def get_speech_file_path(self, input_text):
-		data = "speaker=mijin&speed=0&text=" + unicode(input_text);
+    def get_speech_file_path(self, input_text):
+        data = "speaker=mijin&speed=0&text=" + unicode(input_text);
 
-		q = Request("https://openapi.naver.com/v1/voice/tts.bin")
-		q.add_header("X-Naver-Client-Id", self.client_id)
-		q.add_header("X-Naver-Client-Secret", self.client_secret)
+        q = Request("https://openapi.naver.com/v1/voice/tts.bin")
+        q.add_header("X-Naver-Client-Id", self.client_id)
+        q.add_header("X-Naver-Client-Secret", self.client_secret)
 
-		response = urllib2.urlopen(q, data=data.encode('utf-8'))
-		rescode = response.getcode()
+        response = urllib2.urlopen(q, data=data.encode('utf-8'))
+        rescode = response.getcode()
 
-		if(rescode == 200):
-		    print("Saved TTS to MP3")
-		    response_body = response.read()
+	if(rescode == 200):
+            print("Saved TTS to MP3")
+	    response_body = response.read()
+	    now = datetime.datetime.now()
+	    nowDatetime = now.strftime('%Y%m%d_%H:%M:%S')
+	    filename = str(nowDatetime) + '.mp3'
+	    with open(filename, 'wb') as f:
+	        f.write(response_body)
+                self.play_sound(filename)
+            return filename
 
-		    now = datetime.datetime.now()
-		    nowDatetime = now.strftime('%Y%m%d_%H:%M:%S')
-		    filename = str(nowDatetime) + '.mp3'
-		    with open(filename, 'wb') as f:
-		        f.write(response_body)
-
-		    return filename
-
-		else:
-		    print("Error Code:" + rescode)
+	else:
+	    print("Error Code:" + rescode)
 
--- tts_test.py
+++ tts_test.py
@@ -2,4 +2,6 @@
 import tts
 
 text_to_speech = tts.tts()
-print(text_to_speech.get_speech_file_path(u"오늘은 비가 올 것으로 예상 됩니다."))
+
+your_text = u"오늘 날씨는 맑을것으로 예상 됩니다."
+print(text_to_speech.get_speech_file_path(your_text))
