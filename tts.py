3d3eee6bd6dae7da9b80de4abf4340e6d87b41c1
yoon-jae <yoonjae.cho92@gmail.com>
2017-05-31 22:12:06 +0900

    Add silent front, end of mp3 file

--- tts.py
+++ tts.py
@@ -2,25 +2,24 @@
 import os
 import sys
 import urllib2
-from pygame import mixer, time
 import datetime
-
 from urllib2 import urlopen, Request
 from urlparse import urlparse
+from pygame import mixer, time
+from pydub import AudioSegment
 
 class tts:
     def __init__(self):
         self.client_id = "YOUR_CLIENT_ID"
-	self.client_secret = "YOUR_CLIENT_SECRET"
+        self.client_secret = "YOUR_CLIENT_SECRET"
 
     def play_sound(self, file_path):
         mixer.init(frequency=16000, buffer=24000)
         mixer.music.load(file_path)
-        mixer.music.play(loops=2)
+        mixer.music.play(loops=1)
         while mixer.music.get_busy():
             time.Clock().tick(100)
 
-		
     def get_speech_file_path(self, input_text):
         data = "speaker=mijin&speed=0&text=" + unicode(input_text);
 
@@ -31,17 +30,24 @@ class tts:
         response = urllib2.urlopen(q, data=data.encode('utf-8'))
         rescode = response.getcode()
 
-	if(rescode == 200):
+        if(rescode == 200):
             print("Saved TTS to MP3")
-	    response_body = response.read()
-	    now = datetime.datetime.now()
-	    nowDatetime = now.strftime('%Y%m%d_%H:%M:%S')
-	    filename = str(nowDatetime) + '.mp3'
-	    with open(filename, 'wb') as f:
-	        f.write(response_body)
-                self.play_sound(filename)
-            return filename
+            response_body = response.read()
+            now = datetime.datetime.now()
+            nowDatetime = now.strftime('%Y%m%d_%H:%M:%S')
+            filename = str(nowDatetime) + '.mp3'
+            
+            with open(filename, 'wb') as f:
+                f.write(response_body)
 
-	else:
-	    print("Error Code:" + rescode)
+            song = AudioSegment.from_mp3(filename)
+            second_of_silence = AudioSegment.silent(duration=1000)
+            newsong = second_of_silence + song + second_of_silence
+            newsong.export(filename, format="mp3")
+
+            self.play_sound(filename)
+            return filename
+            
+        else:
+            print("Error Code:" + rescode)
 
--- tts_test.py
+++ tts_test.py
@@ -3,5 +3,5 @@ import tts
 
 text_to_speech = tts.tts()
 
-your_text = u"오늘 날씨는 맑을것으로 예상 됩니다."
+your_text = u"내일의 최고 기온은 28.00 도, 최저 지온은 18.00 도 이며, 구름이 많은 날씨가 예상됩니다."
 print(text_to_speech.get_speech_file_path(your_text))
