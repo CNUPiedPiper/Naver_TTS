d40decd06195cefd2a6a0b82e8cca75ce25827f9
Yoon-jae <yoonjae.cho92@gmail.com>
2017-06-01 14:45:00 +0900

    Adding pad to front of text

--- tts.py
+++ tts.py
@@ -3,10 +3,10 @@ import os
 import sys
 import urllib2
 import datetime
+import pygame
 from urllib2 import urlopen, Request
 from urlparse import urlparse
 from pygame import mixer, time
-from pydub import AudioSegment
 
 class tts:
     def __init__(self):
@@ -14,7 +14,9 @@ class tts:
         self.client_secret = "YOUR_CLIENT_SECRET"
 
     def play_sound(self, file_path):
+        mixer.pre_init(44100, -16, 2, 256)
         mixer.init(frequency=16000, buffer=24000)
+        pygame.init()
         mixer.music.load(file_path)
         mixer.music.play(loops=1)
         while mixer.music.get_busy():
@@ -39,11 +41,6 @@ class tts:
             
             with open(filename, 'wb') as f:
                 f.write(response_body)
-
-            song = AudioSegment.from_mp3(filename)
-            second_of_silence = AudioSegment.silent(duration=1000)
-            newsong = second_of_silence + song + second_of_silence
-            newsong.export(filename, format="mp3")
 
             self.play_sound(filename)
             return filename
--- tts_test.py
+++ tts_test.py
@@ -3,5 +3,5 @@ import tts
 
 text_to_speech = tts.tts()
 
-your_text = u"내일의 최고 기온은 28.00 도, 최저 지온은 18.00 도 이며, 구름이 많은 날씨가 예상됩니다."
+your_text = u"hoo.. 내일의 최고 기온은 28.00 도, 최저 지온은 18.00 도 이며, 구름이 많은 날씨가 예상됩니다."
 print(text_to_speech.get_speech_file_path(your_text))
